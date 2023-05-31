bb079dea6704ba0970b0a7dfc8cf0acce0f50c8d
Daniela Lavric <daniela@hashicorp.com>
2023-05-31 10:58:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ data "tfe_outputs" "my_output" {
 output "my_new_output" {
   value = data.tfe_outputs.my_output.nonsensitive_values
 }
+
+output "my_new_output_server" {
+  value = data.tfe_outputs.my_output.nonsensitive_values.server
+}
